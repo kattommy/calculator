5f2259ce4e1f016bc68a1d5da043fd31d8466d0c
kattommy <katarzynaa.nowak@wp.pl>
2022-07-05 08:34:06 +0200

    added data slicing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-      //  String dataToCalc = scanner.nextLine();
+        //  String dataToCalc = scanner.nextLine();
         String dataToCalc = "1 + 2 -1 + 3 / 2 ";
 
-       // String regex = "^(\\d+[\\+\\-\\*\\/]{1})+\\d+$";
-      String [] dataArray =  dataToCalc.split("[\\+\\-\\*\\/]{1}");
-
-    for(int i = 0; i< dataArray.length; i++){
-        System.out.println(dataArray[i]);
-
-    }
-
-
+        // String regex = "^(\\d+[\\+\\-\\*\\/]{1})+\\d+$";
+        String[] dataArray = dataToCalc.split("[\\+\\-\\*\\/]{1}");
+        for (int i = 0; i < dataArray.length; i++) {
+            System.out.println(dataArray[i]);
+        }
+        System.out.println("---------------");
+        String[] operatorsArray = dataToCalc.split("[0-9]");
+        for (int i = 0; i < operatorsArray.length; i++) {
+            System.out.println(operatorsArray[i]);
+        }
     }
 }
